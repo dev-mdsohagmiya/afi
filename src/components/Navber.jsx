e07c1e51b07cbbc91926562d238360911a2d2d85
msbhh-ms <learnwithmsbhh@gmail.com>
2023-08-23 10:13:36 +0600

    nav update

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, NavLink } from "react-bootstrap";
 // import Manu from "./Manu";
 import "../styles/Navber_Manu.css";
 import Manu from "./Manu";
 
 export default function Navber() {
+  const [profileToggle, setProfiletoggle] = useState("");
+  const [langToggle, setLangToggle] = useState("");
+
   const [toggle, setToggle] = useState("");
   const handleToggle = () => {
     setToggle("toggler-class");
@@ -17,6 +20,29 @@ export default function Navber() {
     setToggle(data);
   };
 
+  const profileClickHandler = () => {
+    console.log("profile card");
+    if (langToggle === "lang-toggle") {
+      setLangToggle("");
+    }
+    if (profileToggle === "profile-toggle") {
+      setProfiletoggle("");
+    } else {
+      setProfiletoggle("profile-toggle");
+    }
+  };
+
+  const langClickHandler = () => {
+    console.log("lang");
+    if (profileToggle === "profile-toggle") {
+      setProfiletoggle("");
+    }
+    if (langToggle === "lang-toggle") {
+      setLangToggle("");
+    } else {
+      setLangToggle("lang-toggle");
+    }
+  };
   return (
     <div>
       <div className="container py-4 ">
@@ -45,47 +71,92 @@ export default function Navber() {
                 <a href="/">AIDE</a>
               </li>
               <li className="d-inline-block  d-xl-inline-block">
-                <div className="lang">
-                  <img
-                    src="assets/flags/france.svg"
-                    className="flag"
-                    alt="kigi"
-                  />
-                  <span className="ml-2">
-                    {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="13"
-                      height="6"
-                      viewBox="0 0 13 6"
-                      fill="none"
-                    >
-                      <path
-                        d="M6.5 6L0.00480895 -2.69824e-07L12.9952 8.65831e-07L6.5 6Z"
-                        fill="#1E1E1E"
-                      />
-                    </svg>
-                  </span>
+                <div className="lang d-inline-block">
+                  <div onClick={langClickHandler} className="pointer">
+                    <img
+                      src="assets/flags/france.svg"
+                      className="flag"
+                      alt="kigi"
+                    />
+                    <span className="ml-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="13"
+                        height="6"
+                        viewBox="0 0 13 6"
+                        fill="none"
+                      >
+                        <path
+                          d="M6.5 6L0.00480895 -2.69824e-07L12.9952 8.65831e-07L6.5 6Z"
+                          fill="#1E1E1E"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <div className={`lang-manu  text-center z-1 ${langToggle}`}>
+                    <div className="">
+                      <NavLink onClick={langClickHandler} className="link">
+                        ENGLISH (ENG)
+                      </NavLink>
+                    </div>
+                    <div>
+                      <NavLink onClick={langClickHandler} className="link">
+                        FRANCH (FR)
+                      </NavLink>
+                    </div>
+                  </div>
                 </div>
               </li>
               <li className="d-inline-block  d-xl-inline-block">
-                <div className="profile">
-                  <img src="assets/profile.svg" className="flag" alt="kigi" />
-                  <span className="">
-                    {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="13"
-                      height="6"
-                      viewBox="0 0 13 6"
-                      fill="none"
-                    >
-                      <path
-                        d="M6.5 6L0.00480895 -2.69824e-07L12.9952 8.65831e-07L6.5 6Z"
-                        fill="#1E1E1E"
-                      />
-                    </svg>
+                <div className="profile ">
+                  <span onClick={profileClickHandler}>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="47"
+                        height="47"
+                        fill="currentColor"
+                        class="bi bi-person"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                      </svg>
+                    </span>
+                    <span className="">
+                      {" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="13"
+                        height="6"
+                        viewBox="0 0 13 6"
+                        fill="none"
+                      >
+                        <path
+                          d="M6.5 6L0.00480895 -2.69824e-07L12.9952 8.65831e-07L6.5 6Z"
+                          fill="#1E1E1E"
+                        />
+                      </svg>
+                    </span>
                   </span>
+                  <div
+                    className={`profile-manu  text-center z-1 ${profileToggle}`}
+                  >
+                    <div className="">
+                      <NavLink onClick={profileClickHandler} className="link">
+                        TABLEAU DE BORD
+                      </NavLink>
+                    </div>
+                    <div>
+                      <NavLink onClick={profileClickHandler} className="link">
+                        PROGRESSION
+                      </NavLink>
+                    </div>
+                    <div>
+                      <NavLink onClick={profileClickHandler} className="link">
+                        SE DÉCONNECTER
+                      </NavLink>
+                    </div>
+                  </div>
                 </div>
               </li>
 
